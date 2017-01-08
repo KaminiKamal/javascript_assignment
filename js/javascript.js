bb39ef2f99fa220e914267ff97a1af60c8ffaa3f
Kamini Kamal <kaminikamaliem@gmail.com>
2017-01-08 08:50:49 +0530

    task

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,12 @@
- 
+ "use strict";
+ function validateForm(){
+ 	alert("hi");
+ 	//var name=document.getElementById("name").value;
+ 	var name=$("#name").val();
+ 	//alert(name);
+ 	var reg=$("#registration").val();
+ 	var address=$("#address").val();
+ 	var state=$("#state").val();
+ 	var roll=$("#roll").val();
+alert(roll);
+ }
